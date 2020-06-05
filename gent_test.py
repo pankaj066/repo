8a0b24f66441f004905a5cd39315aae522124002
pankaj066 <65240253+pankaj066@users.noreply.github.com>
2020-06-05 01:39:42 -0700

    yes did for new test

--- gent_test.py
+++ gent_test.py
@@ -9,7 +9,7 @@ class MyTestCase(unittest.TestCase):
              assert result == "2013"
           return test_year;
 
-     #suite = unittest.TestSuite()
+     ###suite = unittest.TestSuite()
      test_cases = ["2013-05-26", "2013-08-27", "2013-09-28"]
      for case in test_cases:
             suite.addTest(
