e170ac955c2d309cdebdc82f00623c4a652576e0
pankaj066 <65240253+pankaj066@users.noreply.github.com>
2020-06-05 01:36:20 -0700

    final changes

--- gent_test.py
+++ gent_test.py
@@ -19,8 +19,8 @@ class MyTestCase(unittest.TestCase):
 
 
 
-    def test_something(self):
-        self.assertEqual(True, False)
+#    def test_something(self):
+ #       self.assertEqual(True, False)
 
 
 if __name__ == '__main__':
